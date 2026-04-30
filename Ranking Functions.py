859e81b49efbb0de8266668f01eaac474e525b28
Akshar Pandey <pandeyakshar4@gmail.com>
2026-04-30 16:06:04 +0400

    Update Ranking Functions.py
    completed all the functions to rank card hands

--- Ranking Functions.py
+++ Ranking Functions.py
@@ -21,7 +21,7 @@ def royal_flush(deck, table):
             values.append(card[0])
 
         if set(values) == {10, 11, 12, 13, 14}:
-            return True
+            return x
 
     return False
 
@@ -49,9 +49,9 @@ def straight_flush(deck, table):
         special_case = values == [2, 3, 4, 5, 14]
         consecutive_or_nah = values == list(range(values[0], values[-1]+1))
         if consecutive_or_nah and not special_case:
-            return True
+            return x
         elif special_case:
-            return True
+            return x
     return False
 
 def four_of_a_kind(deck, table):
@@ -62,19 +62,162 @@ def four_of_a_kind(deck, table):
         value = []
         for card in combo:
             value.append(card[0])
+        values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 
+        for val in values:
+            count = value.count(val)
+            if count == 4:
+                return x
+    return False
 
+def full_house(deck, table):
+    cards = deck + table
+    combinations1 = list(combinations(cards, 5))
+    for x in combinations1:
+        combo = list(x)
+        value = []
+        for card in combo:
+            value.append(card[0])
 
+        if len(set(value)) > 2:
+            return False
 
+        rank_count = []
+        for rank in set(value):
+            rank_count.append(value.count(rank))
 
-
-
-
-
-
-
-
+        if rank_count in [[3, 2], [2, 3]]:
+            return x
 
     return False
 
-def straight_flush(deck, table):
+
+def flush(deck, table):
+    cards = deck + table
+    combinations1 = list(combinations(cards, 5))
+    for x in combinations1:
+        combo = list(x)
+        suits = []
+        for z in combo:
+            suits.append(z[1])
+
+        if len(set(suits)) == 1:
+            return x
+
+    return False
+
+
+def straight(deck, table):
+    cards = deck + table
+    combinations1 = list(combinations(cards, 5))
+    for x in combinations1:
+        combo = list(x)
+        values = []
+        for card in combo:
+            values.append(card[0])
+
+        values.sort()
+
+        if values == list(range(values[0], values[-1]+1)):
+            return x
+        elif values in [[2, 3, 4, 5, 14], [10, 11, 12, 13, 14]]:
+            return x
+
+    return False
+
+
+def three_of_a_kind(deck, table):
+    cards = deck + table
+    combinations1 = list(combinations(cards, 5))
+    for x in combinations1:
+        combo = list(x)
+        value = []
+        for card in combo:
+            value.append(card[0])
+        values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+        for val in values:
+            count = value.count(val)
+            if count == 3:
+                return x
+
+    return False
+
+
+def two_pair(deck, table):
+    cards = deck + table
+    combinations1 = list(combinations(cards, 5))
+    for x in combinations1:
+        combo = list(x)
+        value = []
+
+        for card in combo:
+            value.append(card[0])
+
+        rank_count = []
+        for rank in set(value):
+            rank_count.append(value.count(rank))
+
+        if rank_count in [[2, 2, 1], [1, 2, 2], [2, 1, 2]]:
+            return x
+
+    return False
+
+
+def one_pair(deck, table):
+    cards = deck + table
+    combinations1 = list(combinations(cards, 5))
+    for x in combinations1:
+        combo = list(x)
+        value = []
+        for card in combo:
+            value.append(card[0])
+        values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+        for val in values:
+            count = value.count(val)
+            if count == 2:
+                return x
+
+    return False
+
+deck = [(8, 'Hearts'), (2, 'Clubs')]
+table = [(8, 'Diamonds'), (5, 'Spades'), (11, 'Hearts'), (4, 'Clubs'), (9, 'Diamonds')]
+
+
+def high_card(deck, table):
+    cards = deck + table
+    combinations1 = list(combinations(cards, 5))
+    best_combo = None
+    for x in combinations1:
+        combo = list(x)
+        if not(best_combo):
+            best_combo = x
+
+        elif x > best_combo:
+            best_combo = x
+
+    return best_combo
+
+deck = [(12, 'Hearts'), (2, 'Clubs')]
+table = [(11, 'Diamonds'), (9, 'Spades'), (8, 'Hearts'), (6, 'Clubs'), (4, 'Diamonds')]
+
+print(high_card(deck, table))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
