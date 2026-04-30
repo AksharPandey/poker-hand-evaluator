96e64b13c5f1b258c42c77733f44ec918206c2ec
Akshar Pandey <pandeyakshar4@gmail.com>
2026-04-30 20:41:09 +0400

    Update main.py
    Completed All cases of comparison, win, lose or tie, added manual card checking, going to start error handling now

--- main.py
+++ main.py
@@ -8,22 +8,40 @@ cards = []
 for x in suits:
     for y in values:
         cards.append((y, x))
-# Getting a Deck of Cards
 
-yn = int(input("Would you like to shuffle the cards 0 - No, 1 - Yes: "))
-if yn:
-    r.shuffle(cards)
+# Getting a Deck of Cards
+r.shuffle(cards)
 
 # Giving the cards to the user and opponent, adding manual card assigning later
 deck1 = []
 deck2 = []
 
-for card in range(2):
-    deck1.append(cards.pop())
-    deck2.append(cards.pop())
+ask = input("Do you wanna randomly generate opponent and your cards  (y/n): ")
+if ask == 'n':
+    value1 = int(input("Enter value for your card1 (1-14), where J - 11, Q - 12, K - 13, A - 14: "))
+    suit1 = input("Enter suit for your card1 (Spades, Hearts, Diamonds, Clubs): ")
+    value2 = int(input("Enter value for your card2 (1-14), where J - 11, Q - 12, K - 13, A - 14: "))
+    suit2 = input("Enter suit for your card2 (Spades, Hearts, Diamonds, Clubs): ")
 
-print(deck1)
-print(deck2)
+    value3 = int(input("Enter value for opp card1 (1-14), where J - 11, Q - 12, K - 13, A - 14: "))
+    suit3 = input("Enter suit for opp card1 (Spades, Hearts, Diamonds, Clubs): ")
+    value4 = int(input("Enter value for opp card2 (1-14), where J - 11, Q - 12, K - 13, A - 14: "))
+    suit4 = input("Enter suit for opp card2 (Spades, Hearts, Diamonds, Clubs): ")
+
+    deck1.append((value1, suit1))
+    deck1.append((value2, suit2))
+    deck2.append((value3, suit3))
+    deck2.append((value4, suit4))
+
+else:
+    for card in range(2):
+        deck1.append(cards.pop())
+        deck2.append(cards.pop())
+
+D1 = str(deck1[0][0]) + ' of ' + deck1[0][1] + '/' + str(deck1[1][0]) + ' of ' + deck1[1][1]
+D2 = str(deck2[0][0]) + ' of ' + deck2[0][1] + '/' + str(deck2[1][0]) + ' of ' + deck2[1][1]
+print(D1)
+print(D2)
 
 sims = int(input("Enter amount of simulations: "))
 rankings = ["RF", "SF", "FK", "FH", "FL", "ST", "TK", "TP", "OP", "HC"]
@@ -113,8 +131,8 @@ for x in range(sims):
                 ranks1.append(rank)
             for rank, suits in strongest_hand2:
                 ranks2.append(rank)
-            ranks1.sort()
-            ranks2.sort()
+            ranks1.sort(reverse=True)
+            ranks2.sort(reverse=True)
             if ranks1 > ranks2:
                 deck1_win +=1
             elif ranks1 == ranks2:
@@ -164,19 +182,226 @@ for x in range(sims):
                 deck2_win += 1
 
         elif rank1 == 'FH' and rank2 == 'FH':
-            tie += 1
+            ranks1 = []
+            ranks2 = []
+            for rank, suits in strongest_hand1:
+                ranks1.append(rank)
+            for rank, suits in strongest_hand2:
+                ranks2.append(rank)
+
+            trip_rank1 = 0
+            trip_rank2 = 0
+            for z in ranks1:
+                if ranks1.count(z) == 3:
+                    trip_rank1 = z
+                    break
+            for z in ranks2:
+                if ranks2.count(z) == 3:
+                    trip_rank2 = z
+                    break
+
+            pair_rank1 = 0
+            pair_rank2 = 0
+            if trip_rank1 > trip_rank2:
+                deck1_win += 1
+            elif trip_rank2 == trip_rank1:
+                for z in ranks1:
+                    if ranks1.count(z) == 1:
+                        pair_rank1 = z
+                        break
+                for z in ranks2:
+                    if ranks2.count(z) == 1:
+                        pair_rank2 = z
+                        break
+                if pair_rank2 > pair_rank1:
+                    deck2_win += 1
+                elif pair_rank2 == pair_rank1:
+                    tie += 1
+                else:
+                    deck1_win += 1
+            else:
+                deck2_win += 1
+
         elif rank1 == 'FL' and rank2 == 'FL':
-            tie += 1
+            ranks1 = []
+            ranks2 = []
+            for rank, suits in strongest_hand1:
+                ranks1.append(rank)
+            for rank, suits in strongest_hand2:
+                ranks2.append(rank)
+            ranks1.sort(reverse=True)
+            ranks2.sort(reverse=True)
+            if ranks1 > ranks2:
+                deck1_win +=1
+            elif ranks1 == ranks2:
+                tie += 1
+            else:
+                deck2_win += 1
+
         elif rank1 == 'ST' and rank2 == 'ST':
-            tie += 1
+            ranks1 = []
+            ranks2 = []
+            for rank, suits in strongest_hand1:
+                ranks1.append(rank)
+            for rank, suits in strongest_hand2:
+                ranks2.append(rank)
+            ranks1.sort(reverse=True)
+            ranks2.sort(reverse=True)
+
+            if ranks1 == [14, 5, 4, 3, 2]:
+                ranks1 = [5, 4, 3, 2, 1]
+            if ranks2 == [14, 5, 4, 3, 2]:
+                ranks2 = [5, 4, 3, 2, 1]
+
+            if ranks1 > ranks2:
+                deck1_win +=1
+            elif ranks1 == ranks2:
+                tie += 1
+            else:
+                deck2_win += 1
+
         elif rank1 == 'TK' and rank2 == 'TK':
-            tie += 1
+            ranks1 = []
+            ranks2 = []
+            for rank, suits in strongest_hand1:
+                ranks1.append(rank)
+            for rank, suits in strongest_hand2:
+                ranks2.append(rank)
+
+            trip_rank1 = 0
+            trip_rank2 = 0
+            for z in ranks1:
+                if ranks1.count(z) == 3:
+                    trip_rank1 = z
+                    break
+            for z in ranks2:
+                if ranks2.count(z) == 3:
+                    trip_rank2 = z
+                    break
+
+            if trip_rank1 > trip_rank2:
+                deck1_win += 1
+            elif trip_rank2 == trip_rank1:
+                two_rank1 = []
+                two_rank2 = []
+                for z in ranks1:
+                    if z != trip_rank1:
+                        two_rank1.append(z)
+                for z in ranks2:
+                    if z != trip_rank2:
+                        two_rank2.append(z)
+
+                two_rank1.sort(reverse=True)
+                two_rank2.sort(reverse=True)
+
+                if two_rank1 > two_rank2:
+                    deck1_win += 1
+                elif two_rank1 == two_rank2:
+                    tie += 1
+                else:
+                    deck2_win += 1
+
+            else:
+                deck2_win += 1
+
         elif rank1 == 'TP' and rank2 == 'TP':
-            tie += 1
+            ranks1 = []
+            ranks2 = []
+            for rank, suits in strongest_hand1:
+                ranks1.append(rank)
+            for rank, suits in strongest_hand2:
+                ranks2.append(rank)
+
+            pair_rank1 = []
+            pair_rank2 = []
+            kicker_rank1 = 0
+            kicker_rank2 = 0
+            for z in ranks1:
+                if ranks1.count(z) == 2:
+                    if not (z in pair_rank1):
+                        pair_rank1.append(z)
+                else:
+                    kicker_rank1 = z
+
+            for z in ranks2:
+                if ranks2.count(z) == 2:
+                    if not (z in pair_rank2):
+                        pair_rank2.append(z)
+                else:
+                    kicker_rank2 = z
+
+            pair_rank2.sort(reverse=True)
+            pair_rank1.sort(reverse=True)
+
+            if pair_rank2 > pair_rank1:
+                deck2_win += 1
+            elif pair_rank1 == pair_rank2:
+                if kicker_rank1 > kicker_rank2:
+                    deck1_win += 1
+                elif kicker_rank2 == kicker_rank1:
+                    tie += 1
+                else:
+                    deck2_win += 1
+            else:
+                deck1_win += 1
+
         elif rank1 == 'OP' and rank2 == 'OP':
-            tie += 1
+            ranks1 = []
+            ranks2 = []
+            for rank, suits in strongest_hand1:
+                ranks1.append(rank)
+            for rank, suits in strongest_hand2:
+                ranks2.append(rank)
+
+            pair_rank1 = 0
+            kicker_rank1 = []
+            pair_rank2 = 0
+            kicker_rank2 = []
+
+            for z in ranks1:
+                if ranks1.count(z) == 2:
+                    pair_rank1 = z
+                else:
+                    kicker_rank1.append(z)
+            for z in ranks2:
+                if ranks2.count(z) == 2:
+                    pair_rank2 = z
+                else:
+                    kicker_rank2.append(z)
+
+            kicker_rank1.sort(reverse=True)
+            kicker_rank2.sort(reverse=True)
+
+            if pair_rank1 > pair_rank2:
+                deck1_win += 1
+
+            elif pair_rank1 == pair_rank2:
+                if kicker_rank1 > kicker_rank2:
+                    deck1_win += 1
+                elif kicker_rank2 > kicker_rank1:
+                    deck2_win += 1
+                else:
+                    tie += 1
+            else:
+                deck2_win += 1
+
         elif rank1 == 'HC' and rank2 == 'HC':
-            tie += 1
+            ranks1 = []
+            ranks2 = []
+            for rank, suits in strongest_hand1:
+                ranks1.append(rank)
+            for rank, suits in strongest_hand2:
+                ranks2.append(rank)
+
+            ranks1.sort(reverse=True)
+            ranks2.sort(reverse=True)
+
+            if ranks1 > ranks2:
+                deck1_win += 1
+            elif ranks2 > ranks1:
+                deck2_win += 1
+            else:
+                tie += 1
 
     else:
         deck1_win += 1
@@ -187,9 +412,12 @@ deck1_win_percent = (deck1_win * 100) / sims
 deck2_win_percent = (deck2_win * 100) / sims
 tie_percent = (tie * 100) / sims
 
-print("Deck1 wins", deck1_win_percent, "percent of times")
-print("Deck2 wins", deck2_win_percent, "percent of times")
-print("Game ties", tie_percent, "percent of times")
+D1 = str(deck1[0][0]) + ' of ' + deck1[0][1] + '/' + str(deck1[1][0]) + ' of ' + deck1[1][1]
+D2 = str(deck2[0][0]) + ' of ' + deck2[0][1] + '/' + str(deck2[1][0]) + ' of ' + deck2[1][1]
+
+print(D1, "wins", deck1_win_percent, "% of times")
+print(D2, "wins", deck2_win_percent, "% of times")
+print("Game ties", tie_percent, "% of times")
 
 
 
